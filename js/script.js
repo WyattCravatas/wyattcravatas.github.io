3a34f1788912e399ad73ffdac1e7949be3381530
WyattCravatas <75994771+WyattCravatas@users.noreply.github.com>
2021-03-23 23:28:10 -0400

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,53 @@
+
+var page = undefined;
+
+function closeMenu() {
+  $(".navbar-collapse").removeClass("show"); 
+}
+
+function highlightMenu(no) {
+  $(".navbar .navbar-nav > .nav-item").removeClass('selected');
+  $(".navbar .navbar-nav > .nav-item > .nav-link[data-no='" + no + "']").parent().addClass('selected');
+}
+
+
+function openPage(no) {
+  if(no == 4) {
+    if(page == undefined) {
+      setupPage();
+    } else {
+      $('.page-slider').slick('unslick');
+      setupPage();
+    }    
+  }
+
+  $('.cd-hero-slider li').hide();
+  $('.cd-hero-slider li[data-page-no="' + no + '"]')
+    .fadeIn();
+}
+
+$(window).on('load', function() {
+  $('body').addClass('loaded');
+  openPage(1);
+});
+
+jQuery(function() {
+    $('.tm-page-link').on('click', function(){
+      var pageNo = $(this).data('page-no');
+      openPage(pageNo);
+      highlightMenu(pageNo);
+    });
+
+    $(".navbar .navbar-nav > .nav-item > a.nav-link").on('click', function(e){
+      var pageNo = $(this).data('no');
+
+      openPage(pageNo);
+      highlightMenu(pageNo);
+      closeMenu();     
+    });
+
+    $("html").click(function(e) {
+      closeMenu();
+    });
+});
+
